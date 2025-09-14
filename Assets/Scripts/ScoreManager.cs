4481f1814f32278c785d92f49332be90e3a4a55e
y-k-web <60056283+y-k-web@users.noreply.github.com>
2025-09-15 00:18:05 +1000

    Randomize item pickup sound

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -205,7 +205,7 @@ public class ScoreManager : MonoBehaviour
             return;
         }
 
-        int index = Mathf.Clamp(chainCount - 1, 0, chainClips.Length - 1);
+        int index = Random.Range(0, chainClips.Length);
         AudioClip clip = chainClips[index];
         if (clip != null)
         {
